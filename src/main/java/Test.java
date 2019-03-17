8a398b282e29020de2c993bc98cd6206b3207893
lzh1508 <38484508+lzh1508@users.noreply.github.com>
2019-03-17 13:30:20 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("test update by Jim ");
         System.out.println("Test.main");
     }
 
