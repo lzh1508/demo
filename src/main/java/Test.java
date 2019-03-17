f0eda4ca4ad02bf77018896c98a5b0548ecaf539
txy670584307 <870557798@qq.com>
2019-03-17 11:34:47 +0800

    dev2

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hc on 2019-03-17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Test.main");
+    }
+
+}
